a531a974adc9eb6f75c03019630e4d0dee0f9d55
Tárek Holanda <12614592+TarekHolanda@users.noreply.github.com>
2023-07-10 16:08:10 -0300

    Fix refresh token method

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,35 +12,47 @@ const GOOGLE_AUTHORIZATION_URL =
 async function refreshAccessToken(token) {
     try {
         const url =
-            "https://oauth2.googleapis.com/token?" +
-            new URLSearchParams({
-                client_id: process.env.GOOGLE_CLIENT_ID,
-                client_secret: process.env.GOOGLE_CLIENT_SECRET,
-                grant_type: "refresh_token",
-                refresh_token: token.refreshToken,
-            });
+            "https://oauth2.googleapis.com/tokeninfo?id_token=" + token.token;
 
         const response = await fetch(url, {
+            method: "GET",
             headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
+                "Content-Type": "application/json",
             },
-            method: "POST",
         });
 
-        const refreshedTokens = await response.json();
+        const currentToken = await response.json();
 
-        if (!response.ok) {
-            throw refreshedTokens;
+        if (currentToken.error === "invalid_token") {
+            const urlRefresh =
+                "https://oauth2.googleapis.com/token?" +
+                new URLSearchParams({
+                    client_id: process.env.GOOGLE_CLIENT_ID,
+                    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+                    grant_type: "refresh_token",
+                    refresh_token: token.access_token,
+                });
+
+            const refreshedResponse = await fetch(urlRefresh, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            });
+
+            const refreshedToken = await refreshedResponse.json();
+
+            return {
+                ...token,
+                token: refreshedToken.id_token,
+                access_token: refreshedToken.access_token,
+            };
         }
 
-        return {
-            ...token,
-            accessToken: refreshedTokens.access_token,
-            accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
-            refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
-        };
+        // In case the token is valid
+        return token;
     } catch (error) {
-        console.log(error);
+        console.error(error);
 
         return {
             ...token,
@@ -73,27 +85,18 @@ export const authOptions = {
             if (account && user) {
                 token = Object.assign({}, token, {
                     token: account.id_token,
-                    accessToken: account.accessToken,
-                    accessTokenExpires: Date.now() + account.expires_in * 1000,
-                    refreshToken: account.refresh_token,
+                    access_token: account.access_token,
                     user,
                 });
             }
-            // Return previous token if the access token has not expired yet
-            if (Date.now() < token.accessTokenExpires) {
-                return token;
-            }
-
-            // Access token has expired, try to update it
             return refreshAccessToken(token);
         },
         async session({ session, token }) {
             if (session) {
                 session = Object.assign({}, session, {
                     token: token.token,
-                    accessToken: token.accessToken,
+                    access_token: token.access_token,
                     user: token.user,
-                    error: token.error,
                 });
             }
             return session;
