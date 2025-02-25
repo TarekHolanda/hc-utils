f2a617de96a5a1f4ce8fcd7e800e68383f39860a
Tárek Holanda <12614592+TarekHolanda@users.noreply.github.com>
2025-02-24 19:46:55 -0800

    Improve Scan and Go performance removing pdf preview

--- app/components/MyUploadBar.js
+++ app/components/MyUploadBar.js
@@ -8,11 +8,7 @@ import UploadIcon from "@mui/icons-material/Upload";
 import DownloadIcon from "@mui/icons-material/Download";
 import DeleteIcon from "@mui/icons-material/Delete";
 import PdfIcon from "@mui/icons-material/PictureAsPdf";
-
-import { PDFDownloadLink } from "@react-pdf/renderer";
-
 import { MySpacer } from "./MySpacer";
-import { MyPdfPreview } from "./MyPdfPreview";
 
 export function MyUploadBar(props) {
     return (
@@ -43,23 +39,18 @@ export function MyUploadBar(props) {
                 Download ZIP
             </Button>
 
-            {props.pdfPreview && (
-                <Box>
-                    <MySpacer size={20} horizontal />
-                    <Button
-                        variant="outlined"
-                        size="large"
-                        component="label"
-                        startIcon={<PdfIcon />}
-                        className="pdf-button">
-                        <PDFDownloadLink
-                            document={props.pdfPreview}
-                            fileName="QR Codes.pdf">
-                            {({}) => "Download PDF"}
-                        </PDFDownloadLink>
-                    </Button>
-                </Box>
-            )}
+            <MySpacer size={20} horizontal />
+
+            <Button
+                variant="outlined"
+                size="large"
+                component="label"
+                onClick={props.togglePDF}
+                startIcon={<PdfIcon />}
+                className="pdf-button">
+                    {props.pdfLoaded ? "Preview Images" : "Preview PDF"}
+            </Button>
+            
 
             <MySpacer size={20} horizontal />
 
